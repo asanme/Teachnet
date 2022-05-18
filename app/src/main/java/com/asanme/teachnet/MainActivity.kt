2fbee22467a52b3683d93a31d9321f45dd503b1a
Alex Sánchez Menage <u.alexsanme@gmail.com>
2022-05-18 11:26:29 +0200

    Update
    Progress made on 17/05/2022

--- app/src/main/java/com/asanme/teachnet/MainActivity.kt
+++ app/src/main/java/com/asanme/teachnet/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
+        super.getActionBar()?.hide()
+
     }
 }
