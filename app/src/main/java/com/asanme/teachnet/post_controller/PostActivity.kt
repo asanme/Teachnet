448cbda231aecd2a8a5b725fe771a1f3fbb3dcab
Alex Sánchez Menage <u.alexsanme@gmail.com>
2022-05-23 14:31:44 +0200

    Update
    Added post screen

---
+++ app/src/main/java/com/asanme/teachnet/post_controller/PostActivity.kt
@@ -0,0 +1,11 @@
+package com.asanme.teachnet.post_controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_screen)
+    }
+}
